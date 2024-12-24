2dd696aebb6de41ee752acf39612e44b8aa16f50
DiptoSarkar182 <diptomanchester2018@gmail.com>
2024-12-24 14:36:47 +0600

    added router push

--- app/categories/[id]/edit/page.js
+++ app/categories/[id]/edit/page.js
@@ -5,8 +5,10 @@ import useGetFetch from "@/app/hooks/useGetFetch";
 import Loader from "@/app/lib/Loader";
 import useUpdateData from "@/app/hooks/useUpdateData";
 import CategoryEditForm from "@/app/components/categories/CategoryEditForm";
+import {useRouter} from "next/navigation";
 
 const page = () => {
+  const router = useRouter();
   const {id} = useParams();
   const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/categories/${id}`
   const {data, loading, error} = useGetFetch(apiUrl)
@@ -15,6 +17,7 @@ const page = () => {
 
     updateData(apiUrl, updatedCategory).then(res => {
       toast.success(res.message)
+      router.push("/categories/all")
     }).catch(err => {
       toast.error(err.message)
     })
--- app/categories/create/page.js
+++ app/categories/create/page.js
@@ -2,15 +2,18 @@
 import usePostData from "@/app/hooks/usePostData";
 import toast from "react-hot-toast";
 import CategoryCreateForm from "@/app/components/categories/CategoryCreateForm";
+import {useRouter} from "next/navigation";
 
 const page = () => {
   const {postData} = usePostData()
   const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/categories`
+  const router = useRouter()
 
   const handleCreateProduct = (formData) => {
     postData(apiUrl, formData)
       .then((response) => {
         toast.success(response.message);
+        router.push("/categories/all")
       })
       .catch((err) => {
         toast.error(err.message);
